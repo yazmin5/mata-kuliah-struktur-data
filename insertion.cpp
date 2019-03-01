668d66587a87c79548d0dd141b80b9c0985ace71
yazmin5 <39585219+yazmin5@users.noreply.github.com>
2019-03-01 10:01:58 +0700

    Create insertion.cpp

---
+++ insertion.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Data{
+	unsigned long nim;
+	string nama;
+	};
+typedef struct Data data;
+
+int n;
+void cetak(data*a){
+	for(int k=0; k<n; k++){
+		cout<<a[k].nim<<" ";
+		}
+		cout <<"\r\n";
+	}
+	
+void Insertion(data*a){
+	for(int i=1; i<n; i++){
+		if(a[i].nim < a[i-1].nim){
+			//buat nuker mundur
+			for(int j=i; j>0; j--){
+				if(a[j].nim < a[j-1].nim)
+					swap(a[j],a[j-1]);
+				else break; 
+			}
+		}
+		cetak(&a[0]);
+	}
+}
+
+int main(){
+	cin >> n;
+	data mhs[n];
+	for(int i=0; i<n; i++){
+		cin >> mhs[i].nim >> mhs[i].nama;
+	}
+	
+	// sorting
+	Insertion(&mhs[0]);
+	
+	// hasil akhir
+	cout << "Hasil akhir :\r\n";
+	for(int k=0; k<n; k++)
+	{
+		cout << mhs[k].nim << " - " << mhs[k].nama;
+		cout << "\r\n";
+	}
+	
+	return 0;
+}
